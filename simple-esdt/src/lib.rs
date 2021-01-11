8e28a1bae38b29222af23e8c92bf7b3d806c6d39
dorin.iancu <dorin.iancu@elrond.com>
2021-01-11 13:22:21 +0200

    handle transfers initiated by CrossChainManagement contract + splitting some code into separate functions

--- simple-esdt/src/lib.rs
+++ simple-esdt/src/lib.rs
@@ -18,6 +18,17 @@ pub trait SimpleEsdt {
 
 	// endpoints - owner-only
 
+	// Take care when calling this, once a name is set, it can't be overwritten
+	#[endpoint(setTokenNameForChain)]
+	fn set_token_name_for_chain_endpoint(&self, chain_id: u64, token_name: BoxedBytes) -> SCResult<()> {
+		only_owner!(self, "Only owner may call this function");
+		require!(self.is_empty_token_name_for_chain(chain_id), "Can't overwrite existing token name!");
+
+		self.set_token_name_for_chain(chain_id, &token_name);
+
+		Ok(())
+	}
+
 	#[endpoint(supplyTokens)]
 	fn supply_tokens(&self) -> SCResult<()> {
 		only_owner!(self, "Only owner may call this function");
@@ -25,9 +36,41 @@ pub trait SimpleEsdt {
 		let token_name = self.get_esdt_token_name_boxed();
 		let wrapped_token_payment = self.get_esdt_value_big_uint();
 
-		let mut total_wrapped = self.get_total_wrapped_remaining(&token_name);
-		total_wrapped += wrapped_token_payment;
-		self.set_total_wrapped_remaining(&token_name, &total_wrapped);
+		self.add_total_wrapped(&token_name, &wrapped_token_payment);
+
+		Ok(())
+	}
+
+	// endpoints - CrossChainManagement contract - only
+
+	#[endpoint(transferEsdtToAccount)]
+	fn transfer_esdt_to_account_endpoint(&self, chain_id: u64, amount: BigUint, to: Address) -> SCResult<()> {
+		require!(self.get_caller() == self.get_cross_chain_management_contract_address(), 
+			"Only the cross chain management contract may call this function");
+
+		let token_name = self.get_token_name_for_chain(chain_id);
+		let total_wrapped = self.get_total_wrapped_remaining(&token_name);
+
+		require!(total_wrapped >= amount, "Contract does not have enough tokens");
+
+		self.transfer_esdt_to_account(&token_name, &amount, &to);
+
+		Ok(())
+	}
+
+	#[endpoint(transferEsdtToContract)]
+	fn transfer_esdt_to_contract_endpoint(&self, chain_id: u64, amount: BigUint, to: Address, 
+		func_name: BoxedBytes, args: Vec<BoxedBytes>) -> SCResult<()> {
+	
+		require!(self.get_caller() == self.get_cross_chain_management_contract_address(), 
+			"Only the cross chain management contract may call this function");
+
+		let token_name = self.get_token_name_for_chain(chain_id);
+		let total_wrapped = self.get_total_wrapped_remaining(&token_name);
+
+		require!(total_wrapped >= amount, "Contract does not have enough tokens");
+
+		self.transfer_esdt_to_contract(&token_name, &amount, &to, &func_name, &args);
 
 		Ok(())
 	}
@@ -63,9 +106,7 @@ pub trait SimpleEsdt {
 		// this should never happen, but we'll check anyway
 		require!(wrapped_egld_payment <= self.get_sc_balance(), "Contract does not have enough funds");
 
-		let mut wrapped_egld_remaining = self.get_total_wrapped_remaining(&wrapped_egld_token_name);
-		wrapped_egld_remaining += &wrapped_egld_payment;
-		self.set_total_wrapped_remaining(&wrapped_egld_token_name, &wrapped_egld_remaining);
+		self.add_total_wrapped(&wrapped_egld_token_name, &wrapped_egld_payment);
 
 		// 1 wrapped eGLD = 1 eGLD, so we pay back the same amount
 		self.send_tx(&self.get_caller(), &wrapped_egld_payment, b"unwrapping");
@@ -84,22 +125,38 @@ pub trait SimpleEsdt {
 		serializer.push_argument_bytes(esdt_token_name.as_slice());
 		serializer.push_argument_bytes(amount.to_bytes_be().as_slice());
 
+		self.substract_total_wrapped(esdt_token_name, amount);
+
 		self.send_tx(&to, &BigUint::zero(), serializer.as_slice());
 	}
 
 	fn transfer_esdt_to_contract(&self, esdt_token_name: &BoxedBytes, amount: &BigUint, to: &Address, 
-		func_name: BoxedBytes, args: Vec<BoxedBytes>) {
+		func_name: &BoxedBytes, args: &Vec<BoxedBytes>) {
 
 		let mut serializer = HexCallDataSerializer::new(ESDT_TRANSFER_STRING);
 		serializer.push_argument_bytes(esdt_token_name.as_slice());
 		serializer.push_argument_bytes(amount.to_bytes_be().as_slice());
 
 		serializer.push_argument_bytes(func_name.as_slice());
-		for arg in &args {
+		for arg in args {
 			serializer.push_argument_bytes(arg.as_slice());
 		}
 
+		self.substract_total_wrapped(esdt_token_name, amount);
+
 		self.async_call(&to, &BigUint::zero(), serializer.as_slice());
+	}
+
+	fn add_total_wrapped(&self, esdt_token_name: &BoxedBytes, amount: &BigUint) {
+		let mut total_wrapped = self.get_total_wrapped_remaining(esdt_token_name);
+		total_wrapped += amount;
+		self.set_total_wrapped_remaining(esdt_token_name, &total_wrapped);
+	}
+
+	fn substract_total_wrapped(&self, esdt_token_name: &BoxedBytes, amount: &BigUint) {
+		let mut total_wrapped = self.get_total_wrapped_remaining(esdt_token_name);
+		total_wrapped -= amount;
+		self.set_total_wrapped_remaining(esdt_token_name, &total_wrapped);
 	}
 
 	// STORAGE
@@ -113,7 +170,7 @@ pub trait SimpleEsdt {
 	#[storage_set("wrappedEgldTokenName")]
 	fn set_wrapped_egld_token_name(&self, token_name: &BoxedBytes);
 
-	// Each chain will have its own token. New types of tokens will be issues/minted as needed by this contract
+	// Each chain will have its own token. New types of tokens will be issued/minted by the owner
 
 	#[view(getTokenNameForChain)]
 	#[storage_get("tokenNameForChain")]
@@ -129,7 +186,7 @@ pub trait SimpleEsdt {
 
 	#[view(getTotalWrapped)]
 	#[storage_get("totalWrappedRemaining")]
-	fn get_total_wrapped_remaining(&self, token_wrapped: &BoxedBytes) -> BigUint;
+	fn get_total_wrapped_remaining(&self, token_name: &BoxedBytes) -> BigUint;
 
 	#[storage_set("totalWrappedRemaining")]
 	fn set_total_wrapped_remaining(&self, token_name: &BoxedBytes , total_wrapped: &BigUint);
