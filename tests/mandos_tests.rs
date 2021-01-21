91e054b6372ef157ff51147c99942351cddbc320
dorin.iancu <dorin.iancu@elrond.com>
2021-01-21 10:53:56 +0200

    testing environment setup

---
+++ tests/mandos_tests.rs
@@ -0,0 +1,40 @@
+extern crate block_header_sync;
+use block_header_sync::*;
+
+extern crate cross_chain_management;
+use cross_chain_management::*;
+
+extern crate esdt_token_manager;
+use esdt_token_manager::*;
+
+use elrond_wasm::*;
+use elrond_wasm_debug::*;
+
+fn contract_map() -> ContractMap<TxContext> {
+	let mut contract_map = ContractMap::new();
+
+	contract_map.register_contract(
+		"file:../BlockHeaderSync/output/block-header-sync.wasm",
+		Box::new(|context| Box::new(BlockHeaderSyncImpl::new(context))),
+	);
+
+	contract_map.register_contract(
+		"file:../CrossChainManagement/output/cross-chain-management.wasm",
+		Box::new(|context| Box::new(CrossChainManagementImpl::new(context))),
+	);
+
+	contract_map.register_contract(
+		"file:../EsdtTokenManager/output/esdt-token-manager.wasm",
+		Box::new(|context| Box::new(EsdtTokenManagerImpl::new(context))),
+	);
+
+	contract_map
+}
+
+#[test]
+fn deploy() {
+	parse_execute_mandos(
+		"mandos/deploy.scen.json",
+		&contract_map(),
+	);
+}
